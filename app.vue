e0037487dfa675a52f00e7dd44c58a145cd636d9
wuyong <biggodw@gmail.com>
2025-02-27 20:30:38 +0800

    调整字体颜色

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class=" bg-white text-black">
     <NuxtLayout>
       <NuxtPage></NuxtPage>
     </NuxtLayout>
--- layouts/default.vue
+++ layouts/default.vue
@@ -5,8 +5,8 @@
         <div class="h-12 w-full"></div>
         <slot></slot>
         <myfooter></myfooter>
-        <ul class="flex justify-around bg-amber-100 fixed 
-       bottom-0 w-full h-10 
+        <ul class="flex justify-around bg-blue-600 fixed left-0
+       bottom-0 w-full h-10 text-2xl font-bold text-white  rounded-xl
        *:align-middle *:leading-10
       ">
             <li>
@@ -16,7 +16,7 @@
                 <NuxtLink to="/fenlei">分类</NuxtLink>
             </li>
             <li>
-                <NuxtLink to="/new">新闻</NuxtLink>
+                <NuxtLink to="/new">文章</NuxtLink>
             </li>
             <li>
                 <NuxtLink to="/about">我们</NuxtLink>
