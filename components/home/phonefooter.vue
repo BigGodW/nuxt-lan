c6e7e0829c58574838b8df6f642e1a61bd553196
wuyong <biggodw@gmail.com>
2025-02-28 11:27:26 +0800

    轮播图优化

---
+++ components/home/phonefooter.vue
@@ -0,0 +1,35 @@
+<template>
+    <ul class="flex justify-around bg-blue-600 fixed left-0
+       bottom-0 w-full h-10 text-sm font-bold text-white  rounded-xl
+       *:align-middle 
+      ">
+        <li>
+            <NuxtLink to="/" class="flex flex-col items-center">
+                <UIcon name="streamline:bank-solid" class="w-5 h-5" />
+                <span>首页</span>
+            </NuxtLink>
+        </li>
+        <li>
+            <NuxtLink to="/fenlei" class="flex flex-col items-center">
+                <UIcon name="streamline:travel-hotel-spa-spa-flower-lotus-lily" class="w-5 h-5" />
+                <span>分类</span>
+            </NuxtLink>
+        </li>
+        <li>
+
+            <NuxtLink to="/new" class="flex flex-col items-center">
+                <UIcon name="streamline:annoncement-megaphone" class="w-5 h-5" />
+                <span>新闻</span>
+            </NuxtLink>
+
+        </li>
+        <li>
+
+            <NuxtLink to="/about" class="flex flex-col items-center">
+                <UIcon name="streamline:backpack" class="w-5 h-5" />
+                <span>我们</span>
+            </NuxtLink>
+
+        </li>
+    </ul>
+</template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -4,39 +4,7 @@
         <!-- 顶部空余 -->
         <div class="h-12 w-full"></div>
         <slot></slot>
+        <HomePhonefooter class=" md:hidden"></HomePhonefooter>
         <myfooter></myfooter>
-        <ul class="flex justify-around bg-blue-600 fixed left-0
-       bottom-0 w-full h-10 text-sm font-bold text-white  rounded-xl
-       *:align-middle 
-      ">
-            <li>
-                <NuxtLink to="/" class="flex flex-col items-center">
-                    <UIcon name="streamline:bank-solid" class="w-5 h-5" />
-                    <span>首页</span>
-                </NuxtLink>
-            </li>
-            <li>
-                <NuxtLink to="/fenlei" class="flex flex-col items-center">
-                    <UIcon name="streamline:travel-hotel-spa-spa-flower-lotus-lily" class="w-5 h-5" />
-                    <span>分类</span>
-                </NuxtLink>
-            </li>
-            <li>
-                <NuxtLink to="/new">
-                    <NuxtLink to="/fenlei" class="flex flex-col items-center">
-                        <UIcon name="streamline:annoncement-megaphone" class="w-5 h-5" />
-                        <span>新闻</span>
-                    </NuxtLink>
-                </NuxtLink>
-            </li>
-            <li>
-                <NuxtLink to="/about">
-                    <NuxtLink to="/fenlei" class="flex flex-col items-center">
-                        <UIcon name="streamline:backpack" class="w-5 h-5" />
-                        <span>我们</span>
-                    </NuxtLink>
-                </NuxtLink>
-            </li>
-        </ul>
     </div>
 </template>
